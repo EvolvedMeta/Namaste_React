6eb665a6475bd160dae63bc3a61cbbe8f92a8d65
EvolvedMeta <yashbindal815@gmail.com>
2024-01-04 23:19:14 +0530

    chore: basic react dom manipulation part 2

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-const heading = React.createElement("h1", {id:"heading", attr:'xyc'}, "Hello World from React!");
-console.log(heading); //Will Print the object ----> {$$typeof: Symbol(react.element), type: 'h1', key: null, ref: null, props: {…}, …}
 /**
  * If we try to print heading element it will return the object
  * {type:{}, Props:{}, and so on}
  * type have the HTML tag data like h1,img and so on
  * Props have the attribute details and children have the data that needs to printed
  */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "h1",
+      { id: "heading", key: "head" },
+      "First Heading"
+    ),
+    React.createElement(
+        "h3",
+        { id: "subHead", key: "subHead" },
+        "Sub Heading"
+      ),
+  ])
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); //render will take the heading object then covert it into the html elemnent
+root.render(parent); //render will take the heading object then covert it into the html elemnent
