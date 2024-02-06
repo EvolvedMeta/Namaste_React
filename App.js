d18c2e4e218fcbbeb13f61b7a60ced38321eac86
EvolvedMeta <yashbindal815@gmail.com>
2024-02-06 06:49:32 +0530

    chore: chapter 2 refresher

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * If we try to print heading element it will return the object
- * {type:{}, Props:{}, and so on}
- * type have the HTML tag data like h1,img and so on
- * Props have the attribute details and children have the data that needs to printed
- */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "h1",
-      { id: "heading", key: "head" },
-      "First Heading"
-    ),
-    React.createElement(
-        "h3",
-        { id: "subHead", key: "subHead" },
-        "Sub Heading"
-      ),
-  ])
-);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); //render will take the heading object then covert it into the html elemnent
+//React Element
+const heading = React.createElement("h1", {id:"heading"}, "Namaste React");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
