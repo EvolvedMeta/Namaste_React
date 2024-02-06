babcdf346aa0b0fbe512042f4129f6bbf66698a1
EvolvedMeta <yashbindal815@gmail.com>
2024-02-06 07:13:13 +0530

    basic jsx

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element
-const heading = React.createElement("h1", {id:"heading"}, "Namaste React");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+console.log(heading);
+
+//JSX React - is not HTML in JS, HTML-like or XML-like syntax
+//JSX (transpiled before it  reaches the JS) - PARCEL - Babel
+//JSX => React.createElement => ReactElement- JS Object => HtmlElement(render)
+
+const jsxHeading = <h1 id="heading" className="head" tabIndex={1}>Namaste React using JSX</h1>;
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+
