843f4ee3d1694ea392d6b9c4b1fb3de0ffe36519
EvolvedMeta <yashbindal815@gmail.com>
2024-02-24 06:21:22 +0530

    chore: render component

--- App.js
+++ App.js
@@ -5,20 +5,17 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 console.log(heading);
 
-const jsxHeading = (<h1 id="heading" className="head" tabIndex={5}>Namaste React using JSX</h1>)
-console.log(jsxHeading);
+const Title = () => (<h1 id="heading" className="head" tabIndex={5}>Namaste React using JSX</h1>)
 
-// React Functional Component
-const HeadingComponent1 = () => { return <h1>Namaste React Functional Component</h1>};
+const HeadingComponent1 = () => { return <h1>Namaste React Functional Component</h1> };
 const HeadingComponent2 = () => (
     <div id="container">
+        <Title/>
         <h1>Namaste React Functional Component</h1>
     </div>
 );
 
 
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent2/>);
+root.render(<HeadingComponent2 />);
 
